6d75cf5b1b7afa8194e2f7ad20678bf7b4e114ad
Scott D'Angelo <scott.dangelo@gmail.com>
2017-06-01 14:51:49 -0600

    Initial commit

---
+++ Assets/Scripts/CreatableObject.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreatableObject : MonoBehaviour {
+
+	[Header("Creatable Object", order = 0)]
+
+	public string id;
+	public string matKey;
+
+	public bool isGrabbed = false;
+	public float throwVelocity = 10.0f;
+
+	public bool isCustomizable = false;
+	public bool isLarge = false;
+	public bool isFreezable = true;
+	public bool isFood = false;
+	public float minScale;
+	public float maxScale;
+	public float minDistance;
+	public float maxDistance;
+	public AudioClip collisionClip;
+	public AudioClip consumeClip;
+
+	[Header("Materials Dictionary", order = 1)]
+	public List<string> _mKeys = new List<string>();
+	public List<Material> _mValues = new List<Material>();
+	protected Dictionary<string, Material> mats;
+
+	protected bool defaultPrecision;
+	public bool isFrozen = false;
+
+	protected Vector3 initialScale;
+
+	private GameObject grabbingObject;
+	private bool originalKinematicState;
+
+	#region InitAndLifecycle
+	//------------------------------------------------------------------------------------------------------------------
+	// Initialization and Lifecycle
+	//------------------------------------------------------------------------------------------------------------------
+
+	// Use this for initialization
+	protected virtual void Start () 
+	{
+		initialScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () 
+	{
+		if (transform.position.y < -50)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	protected virtual void InitializeMaterials()
+	{
+		mats = new Dictionary<string, Material>();
+
+		for (var i = 0; i != Math.Min(_mKeys.Count, _mValues.Count); i++)
+		{
+			mats.Add(_mKeys[i], _mValues[i]);
+		}
+	}
+	#endregion
+
+	#region Modifying Object
+	//------------------------------------------------------------------------------------------------------------------
+	// Modifying Object
+	//------------------------------------------------------------------------------------------------------------------
+	public virtual void ApplyMaterial(string matKey)
+	{ 
+		if (mats == null)
+		{
+			InitializeMaterials();
+		}
+
+		if (mats.ContainsKey(matKey))
+		{
+			Material mat = mats[matKey];
+			GetComponent<Renderer>().material = mat;
+		} else
+		{
+			Debug.Log("Invlid Material for Object");
+		}
+	}
+
+	public virtual void ApplyScale(string scale)
+	{
+		float currentMass = GetComponent<Rigidbody>().mass;
+		if (scale == "big")
+		{
+			if (maxScale > 0)
+			{
+				transform.localScale = new Vector3(maxScale, maxScale, maxScale);
+				GetComponent<Rigidbody>().mass = currentMass * maxScale * 10;
+			} else
+			{
+				transform.localScale = new Vector3(transform.localScale.x * 2, transform.localScale.y * 2, transform.localScale.z * 2);
+				GetComponent<Rigidbody>().mass = currentMass * 20;
+			}
+		} else if (scale == "small")
+		{
+			if (minScale > 0)
+			{
+				transform.localScale = new Vector3(minScale, minScale, minScale);
+				GetComponent<Rigidbody>().mass = currentMass / (minScale * 10);
+			} else
+			{
+				transform.localScale = new Vector3(transform.localScale.x / 2, transform.localScale.y / 2, transform.localScale.z / 2);
+				GetComponent<Rigidbody>().mass = currentMass / 20;
+			}
+		}
+	}
+	#endregion
+
+	#region Touching
+	//------------------------------------------------------------------------------------------------------------------
+	// Touching Object
+	//------------------------------------------------------------------------------------------------------------------
+	public virtual void Touch(GameObject touchingObject) {
+
+	}
+
+	public virtual void StopTouch(GameObject touchingObject) {
+
+	}
+
+	#endregion
+
+	#region Grabbing
+	//------------------------------------------------------------------------------------------------------------------
+	// Grabbing Object
+	//------------------------------------------------------------------------------------------------------------------
+
+	public virtual void Grab(GameObject grabbingObject) {
+		this.grabbingObject = grabbingObject;
+		gameObject.transform.parent = grabbingObject.transform;
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		originalKinematicState = rb.isKinematic;
+		rb.isKinematic = true;
+		//grabbingObject.GetComponent<ControllerListener> ().isHolding = true;
+		isGrabbed = true;
+	}
+
+	public virtual void Release(GameObject grabbingObject) {
+		this.grabbingObject = null;
+		gameObject.transform.parent = null;
+		GetComponent<Rigidbody> ().isKinematic = originalKinematicState;
+		//grabbingObject.GetComponent<ControllerListener> ().isHolding = false;
+		isGrabbed = false;
+		//ThrowReleasedObject ();
+	}
+
+	private void ThrowReleasedObject()
+	{
+		Transform origin = gameObject.transform;
+		Vector3 angularVelocity = GvrController.Gyro;
+		Rigidbody rigidBody = GetComponent<Rigidbody>();
+
+		Debug.Log ("GYRO: " + angularVelocity);
+
+		if (origin != null)
+		{
+			rigidBody.velocity = new Vector3 (throwVelocity, throwVelocity, throwVelocity);
+			rigidBody.angularVelocity = origin.TransformDirection(angularVelocity);
+		}
+		else
+		{
+			rigidBody.velocity = new Vector3 (throwVelocity, throwVelocity, throwVelocity);
+			rigidBody.angularVelocity = angularVelocity;
+		}
+	}
+
+	#endregion
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
+using System.IO;
+
+public class GameManager : MonoBehaviour {
+
+	[Header("Objects Dictionary", order = 0)]
+	public List<string> _oKeys = new List<string>();
+	public List<GameObject> _oValues = new List<GameObject>();
+	protected Dictionary<string, GameObject> objects;
+	
+	[Header("AudioSources", order = 1)]
+	public GvrAudioRoom musicSource;
+	public GvrAudioSource voiceSource;
+	public GvrAudioSource errorSource;
+
+	[Header("Objects", order = 2)]
+	public GameObject mainCamera;
+	
+    [Header("Pointer", order = 1)]
+    public GvrReticlePointer pointer;
+    
+	protected GameObject pointerTarget;
+    private GameObject currentObject;
+    private  GameObject[] gameObjects;
+
+    #region InitAndLifecycle
+    //------------------------------------------------------------------------------------------------------------------
+    // Initialization and Lifecycle
+    //------------------------------------------------------------------------------------------------------------------
+
+    protected virtual void Awake()
+	{
+        InitializeObjects();
+    }
+		
+	protected virtual void Start () {
+
+	}
+
+	protected virtual void Update () {
+
+	}
+
+	void InitializeObjects()
+	{
+		Debug.Log ("Initializing Objects");
+		objects = new Dictionary<string, GameObject>();
+
+		for (var i = 0; i != Math.Min(_oKeys.Count, _oValues.Count); i++)
+		{
+			objects.Add(_oKeys[i], _oValues[i]);
+		}
+			
+		_oValues.Clear();
+	}
+
+	#endregion
+
+	#region Public
+	//------------------------------------------------------------------------------------------------------------------
+	// Public Interface Functions
+	//------------------------------------------------------------------------------------------------------------------
+
+	public void PlayClip(AudioClip clip)
+	{
+		voiceSource.clip = clip;
+		voiceSource.Play();
+	}
+
+	public void PlayError(AudioClip clip)
+	{
+		if (!errorSource.isPlaying && !voiceSource.isPlaying)
+		{
+			errorSource.clip = clip;
+			errorSource.Play();
+		}
+	}
+
+    private GameObject FindClosestObject()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Creatable");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = pointer.reticlePointerImpl.GetLineEndPoint();
+
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+    public void DestroyAtPointer() 
+	{
+        pointerTarget = null;
+        pointerTarget = FindClosestObject();
+        if (pointerTarget) {
+			Destroy (pointerTarget);
+		}
+	}
+
+	public virtual void CreateObject(string key, string matKey, string scale)
+	{
+		GameObject newObject = objects[key];
+
+        // Check Pointer
+        Vector3? tempDestination = pointer.reticlePointerImpl.GetLineEndPoint();
+        
+     	if (newObject != null && tempDestination != null)
+		{
+			Vector3 destination = (Vector3)tempDestination;
+			Vector3 location = new Vector3(destination.x, destination.y + newObject.transform.position.y, destination.z);
+       
+			if (matKey != null && newObject.GetComponent<CreatableObject>().isCustomizable)
+			{
+				newObject.GetComponent<CreatableObject>().ApplyMaterial(matKey);
+				newObject.GetComponent<CreatableObject>().matKey = matKey;
+			}
+            
+			GameObject objectInstance = (GameObject) Instantiate(newObject, location, newObject.transform.rotation);
+
+			if (scale != null)
+			{
+				objectInstance.GetComponent<CreatableObject>().ApplyScale(scale);
+			}
+		}
+	}
+
+    #endregion
+}
